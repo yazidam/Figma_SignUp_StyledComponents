a3a8a85922f0f62c0f0a44c754d7caeca594623a
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2022-01-29 10:17:59 +0100

    Adding logo to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,48 @@
 import React from "react";
 import styled from "styled-components";
-
+import logo from "../assets/logo.svg";
 const Sidebar = () => {
-  return <Container>dddd</Container>;
+  return (
+    <Container>
+      <LogoWrapper>
+        <img src={logo} />
+        <h3>
+          Eli <span>Codes</span>
+        </h3>
+      </LogoWrapper>
+      <Form>
+        <h3>Sign Up</h3>
+      </Form>
+    </Container>
+  );
 };
 
 export default Sidebar;
-
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h3 {
+    color: #666666;
+    margin-bottom: 2rem;
+  }
+`;
+const LogoWrapper = styled.div`
+  img {
+    height: 6rem;
+  }
+  h3 {
+    color: #ff8d8d;
+    text-align: center;
+    font-size: 22px;
+  }
+  span {
+    color: #5dc399;
+    font-weight: 300;
+    font-size: 18px;
+  }
+`;
 const Container = styled.div`
   min-width: 400px;
   backdrop-filter: blur(35px);
